83097c31caa53d54c0dc9d5ac8a2fface0ba2548
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-09-01 16:34:05 -0300

    #Commit

--- src/test/java/com/tuyo/SpringSwaggerApplicationTests.java
+++ src/test/java/com/tuyo/SpringSwaggerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MigrationflywayspringApplicationTests {
+class SpringSwaggerApplicationTests {
 
 	@Test
 	void contextLoads() {
